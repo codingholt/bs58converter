73c7c4ccb3d15b3238d7909cf071c8e7cf4f151c
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-19 22:44:27 +0100

    decode working, next on encoding

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ program
   .name('bs58')
   .description('CLI to encode or decode base 58')
   .version('0.0.1');
-
+/*
 program
     .command('encode')
     .description('Encode a array to base 58 string')
-    .argument('input', 'array to encode')
+    .option('input', 'array to encode')
     .action((input) => {
         const array = Uint8Array.from(input)
         const encoded = bs58.encode(array)
@@ -21,7 +21,7 @@ program
         console.log('')
         console.log(`bs58 encoded: ${encoded}`)
       });
-
+*/
 program
       .command('decode')
       .description('Decode a string to bytes')
@@ -35,28 +35,3 @@ program
         });
   
 program.parse()
-
-// rl.question('Encode or Decode?', input =>{
-//     if(input == 'encode'){
-//         console.log('')
-//         rl2.question('Input here please: '), input =>{
-//             const array = Uint8Array.from(input)
-//             const encoded = bs58.encode(array)
-//             console.log('')
-//             console.log('-----')
-//             console.log('')
-//             console.log(`bs58 encoded: ${encoded}`)
-//         }
-        
-//     }else if (input == 'decode'){
-//         console.log('')
-//         rl2.question('Input here please:  ', input =>{
-//             const decoded = bs58.encode(input)
-//             console.log('')
-//             console.log('-----')
-//             console.log('')
-//             console.log(`bs58 decoded: ${decoded}`)
-//         })
-       
-//     }
-// })
